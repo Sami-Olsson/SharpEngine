70c00c4067eeb724833498722869181e370cc721
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-09 11:30:07 +0100

    Add Triangel Vertices

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GLFW;
+using static OpenGL.Gl;
 
 namespace SharpEngine
 {
@@ -7,17 +8,46 @@ namespace SharpEngine
     { 
         
         
-        static void Main(string[] args)
+        static void Main()
         {
             Glfw.Init();
+          
 
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
+            Glfw.MakeContextCurrent(window); 
+            Import(Glfw.GetProcAddress); 
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
 
-           while (!Glfw.WindowShouldClose(window))
+            float[] vertices = new float[] {
+                -.5f, -.5f, 0f,
+                .5f, -.5f, 0f,
+                0f, .5f, 0f
+            };
+
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            unsafe {
+                fixed (float* vertex = &vertices[0]) {
+                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+                }
+                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+            }
+            glEnableVertexAttribArray(0);
+
+            while (!Glfw.WindowShouldClose(window))
             {
                 Glfw.PollEvents(); // react to window changes
-                //do nothing
+                glDrawArrays(GL_TRIANGLES, 0, 3); 
+                Glfw.SwapBuffers(window);
+                
+                
             }
         }
     }
