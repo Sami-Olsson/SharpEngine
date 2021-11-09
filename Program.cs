11c8e56082b09d8807c8314c890a88f5add6f0f4
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-09 14:51:12 +0100

    Clean up the code using methods

--- Program.cs
+++ Program.cs
@@ -8,22 +8,24 @@ namespace SharpEngine
     class Program
     {
         static void Main(string[] args) {
-            // initialize and configure
-            Glfw.Init();
-            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
-            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
-            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
-            Glfw.WindowHint(Hint.Decorated, true);
-            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
-            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
-            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
-
-            // create and launch a window
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
-            Import(Glfw.GetProcAddress);
             
-            float[] vertices = new float[] {
+            var window = Window();
+            CreatShaderProgram();
+            LoadTriangleIntoBuffer();
+
+
+            // engine rendering loop
+            while (!Glfw.WindowShouldClose(window)) {
+                Glfw.PollEvents(); // react to window changes (position etc.)
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+                glFlush();
+            }
+        }
+
+        private static unsafe void LoadTriangleIntoBuffer()
+        {
+            float[] vertices = new float[]
+            {
                 -.5f, -.5f, 0f,
                 .5f, -.5f, 0f,
                 0f, .5f, 0f
@@ -34,40 +36,55 @@ namespace SharpEngine
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            unsafe {
-                fixed (float* vertex = &vertices[0]) {
+            unsafe
+            {
+                fixed (float* vertex = &vertices[0])
+                {
                     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
                 }
+
                 glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
             }
+
             glEnableVertexAttribArray(0);
+        }
 
-
-
-
+        private static void CreatShaderProgram()
+        {
             // create vertex shader
             var vertexShader = glCreateShader(GL_VERTEX_SHADER);
             glShaderSource(vertexShader, File.ReadAllText("shaders/red-triangle.vert"));
             glCompileShader(vertexShader);
-            
+
             // create fragment shader
             var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
             glShaderSource(fragmentShader, File.ReadAllText("shaders/red-triangle.frag"));
             glCompileShader(fragmentShader);
-            
+
             // create shader program - rendering pipeline
             var program = glCreateProgram();
             glAttachShader(program, vertexShader);
             glAttachShader(program, fragmentShader);
             glLinkProgram(program);
             glUseProgram(program);
-            
-            // engine rendering loop
-            while (!Glfw.WindowShouldClose(window)) {
-                Glfw.PollEvents(); // react to window changes (position etc.)
-                glDrawArrays(GL_TRIANGLES, 0, 3);
-                glFlush();
-            }
+        }
+
+        private static Window Window()
+        {
+            Glfw.Init();
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
+
+            // create and launch a window
+            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, GLFW.Window.None);
+            Glfw.MakeContextCurrent(window);
+            Import(Glfw.GetProcAddress);
+            return window;
         }
     }
 }
