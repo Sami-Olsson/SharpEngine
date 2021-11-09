8575e2d6509ab0f25e2460953a097f1fe7f280a2
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-09 09:54:54 +0100

    Add Window Initialization Code to Main

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using GLFW;
 
 namespace SharpEngine
 {
-    class Program
-    {
+    class Program 
+    { 
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var window = new NativeWindow(800, 600, "MyWindowTitle"))
+            {
+                // Main application loop
+                while (!window.IsClosing)
+                {
+                    // OpenGL rendering
+                    // Implement any timing for flow control, etc (see Glfw.GetTime())
+        
+                    // Swap the front/back buffers
+                    window.SwapBuffers();
+        
+                    // Poll native operating system events (must be called or OS will think application is hanging)
+                    Glfw.PollEvents();
+                }
+            }
         }
     }
 }
