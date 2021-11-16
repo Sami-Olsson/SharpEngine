b0c712d7b8b7c6b81dc1d53b68f37dec034c9535
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-11-16 16:41:01 +0100

    Shape, Circle and Rectangle

--- Program.cs
+++ Program.cs
@@ -1,93 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using GLFW;
 
 namespace SharpEngine
 {
-    class Program {
-        static float Lerp(float from, float to, float t) {
+    class Program
+    {
+        static float Lerp(float from, float to, float t)
+        {
             return from + (to - from) * t;
         }
 
-        static float GetRandomFloat(Random random, float min = 0, float max = 1) {
-            return Lerp(min, max, (float)random.Next() / int.MaxValue);
+        static float GetRandomFloat(Random random, float min = 0, float max = 1)
+        {
+            return Lerp(min, max, (float) random.Next() / int.MaxValue);
         }
-        
-        static void FillSceneWithTriangles(Scene scene, Material material) {
-            var random = new Random();
-            for (var i = 0; i < 10; i++) {
-                var triangle = new Triangle(new Vertex[] {
-                    new Vertex(new Vector(-.1f, 0f), Color.Red),
-                    new Vertex(new Vector(.1f, 0f), Color.Green),
-                    new Vertex(new Vector(0f, .133f), Color.Blue)
-                }, material);
-                triangle.Transform.Rotate(GetRandomFloat(random));
-                triangle.Transform.Move(new Vector(GetRandomFloat(random, -1, 1), GetRandomFloat(random, -1, 1)));
-                scene.Add(triangle);
-            }
-        }
-        
-        static void Main(string[] args) {
-            
+
+        static void Main(string[] args)
+        {
+
             var window = new Window();
             var material = new Material("shaders/world-position-color.vert", "shaders/vertex-color.frag");
             var scene = new Scene();
             window.Load(scene);
 
-            //FillSceneWithTriangles(scene, material);
-            var newTriangle = new Triangle(new Vertex[] {
-                new Vertex(new Vector(-.1f, 0f), Color.Red),
-                new Vertex(new Vector(.1f, 0f), Color.Green),
-                new Vertex(new Vector(0f, .133f), Color.Blue)
-            }, material);
-            scene.Add(newTriangle);
-            
-            // engine rendering loop
-            var direction = new Vector(0.01f, 0.01f);
-            var multiplier = 0.95f;
-            var rotation = 0.05f;
-            const int fixedStepNumberPerSecond = 30;
-            const double fixedStepDuration = 1.0 / fixedStepNumberPerSecond;
-            double previousFixedStep = 0.0;
-            
-            
-            while (window.IsOpen())
-            {
+            var shape = new Triangle(material);
+            shape.Transform.CurrentScale = new Vector(0.5f, 1f, 1f);
+            scene.Add(shape);
 
-                // Update Triangles
-                for (var i = 0; i < scene.triangles.Count; i++) {
-                    var triangle = scene.triangles[i]; 
-                    
-                    if (Glfw.Time > previousFixedStep + fixedStepDuration)
-                        previousFixedStep = Glfw.Time;
-                
-                    // 2. Keep track of the Scale, so we can reverse it
-                    if (triangle.Transform.CurrentScale.GetMagnitude() <= 0.5f) {
-                        multiplier = 1.05f;
+            var ground = new Rectangle(material);
+            ground.Transform.CurrentScale = new Vector(10f, 1f, 1f);
+            ground.Transform.Position = new Vector(0f, -1f);
+            scene.Add(ground);
+
+            // engine rendering loop
+            const int fixedStepNumberPerSecond = 30;
+            const float fixedStepDuration = 1.0f / fixedStepNumberPerSecond;
+            
+            double previousFixedStep = 0.0;
+            while (window.IsOpen()) {
+                while (Glfw.Time > previousFixedStep + fixedStepDuration) {
+                    previousFixedStep += fixedStepDuration;
+
+                    if (window.GetKey(Keys.W)) {
+                        shape.Transform.Position += new Vector(0f, 0.5f * fixedStepDuration);
                     }
-                    if (triangle.Transform.CurrentScale.GetMagnitude() >= 2f) {
-                        multiplier = 0.95f;
+                    if (window.GetKey(Keys.S)) {
+                        shape.Transform.Position += new Vector(0f, -0.5f * fixedStepDuration);
                     }
-                    
-                    triangle.Transform.Scale(multiplier);
-                    triangle.Transform.Rotate(rotation);
-                
-                    // 4. Check the X-Bounds of the Screen
-                    if (triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0) {
-                        direction.x *= -1;
+                    if (window.GetKey(Keys.A)) {
+                        shape.Transform.Position += new Vector(-0.5f * fixedStepDuration, 0f);
                     }
-                
-                    // 5. Check the Y-Bounds of the Screen
-                    if (triangle.GetMaxBounds().y >= 1 && direction.y > 0 || triangle.GetMinBounds().y <= -1 && direction.y < 0) {
-                        direction.y *= -1;
+                    if (window.GetKey(Keys.D)) {
+                        shape.Transform.Position += new Vector(0.5f * fixedStepDuration, 0f);
                     }
-                    
-                    
-                    triangle.Transform.Move(direction);
                 }
-                
                 window.Render();
             }
         }
     }
-}
+} 
